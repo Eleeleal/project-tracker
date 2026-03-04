c6a56c9727f7ae7d845bf1c437d2350b8066327d
Eleeleal <gabrieldorus06@gmail.com>
2026-03-04 19:50:03 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Ganti dengan konfigurasi Firebase kamu
 const firebaseConfig = {
   apiKey: "AIzaSyCUT8M82v7zHyM-8ZFQKnskMq49Bq1xr0U",
   authDomain: "project-tracker-f1c0c.firebaseapp.com",
@@ -8,6 +7,9 @@ const firebaseConfig = {
   appId: "1:1073499050735:web:e630f1441ce684f42dc11a"
 };
 
+// Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Auth & Firestore
 const auth = firebase.auth();
 const db = firebase.firestore();
